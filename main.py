d6c784f55f8c7e0155022600fe22cbd5533b6d5a
Kalashnik <kalasmaks97@yandex.ru>
2022-11-17 18:35:42 +0400

    Много принтов

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 def main():
     print('My best git program')
     print("Я научился комитить")
+<<<<<<< HEAD
 
+=======
+    print("Я научился пушать")
+>>>>>>> 1d97777 (Много принтов)
 
 if __name__ == '__main__':
     main()
